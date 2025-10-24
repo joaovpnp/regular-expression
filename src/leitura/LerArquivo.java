74a2a9470191e20ed0abec117ae2d8101e0f5c91
Paiva <joaov.pnp@gmail.com>
2025-10-24 17:05:22 -0300

    Removida função inutilizada

--- src/leitura/LerArquivo.java
+++ src/leitura/LerArquivo.java
@@ -5,23 +5,9 @@ import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivo {
-
-    public static List<String> lerPalavras(String caminho) throws FileNotFoundException {
-
-        List<String> palavras = new LinkedList<>();
-        Scanner file = new Scanner(new File(caminho));
-
-        while (file.hasNextLine())
-            palavras.add(file.nextLine());
-
-        file.close();
-        return palavras;
-    }
 
     public static String lerPalavraMealy(int ordem) throws FileNotFoundException {
         Scanner file = new Scanner(new File("src/arquivos/palavrasMealy/w" + ordem + ".txt"));
